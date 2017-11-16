690c7e628a1a83feeb2cf9044f3b0509bf640371
herenoch <herenoch15@yahoo.com>
2017-11-16 14:36:23 +0300

    Mise a jours page services

--- mobilesys_application/views/Services/view_services.php
+++ mobilesys_application/views/Services/view_services.php
@@ -15,13 +15,16 @@
                 </div>
             </div>
             <div class="row detailllepage listservice">
-                <div class="col-md-4 col-sm-12 imagesServices">
+                <div class="col-md-4 col-sm-10 col-xs-12 imagesServices">
                     <img src="<?php print (site_url("assets/images/developpement-web1.jpg")); ?>">
                 </div>
                 <div class="col-md-8 col-sm-12">
                    <p class="detailleServices">Conception site web et développement : site professionnel, site vitrine, site e-commerce (site prestashop, magento marketplace...), site spécifique… | Intégration sur mobile, desktop et site responsive design | Création de visuel : charte graphique, logo… Notre équipe de développement web est à la hauteur de toutes vos demandes : de la fonctionnalité la plus courante aux plus improbables.</p>
-                    <div class="row">
-                        <div class="col-sm-2"></div>
+                    <div class="row panelImagesappServ">
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/symfony_black.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/symfony_black.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/symfony_black.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/symfony_black.jpg")); ?>"></div></div>
                     </div>
                 </div>
             </div>
@@ -33,13 +36,16 @@
                 </div>
             </div>
             <div class="row detailllepage listservice">
-                <div class="col-md-4 col-sm-12 imagesServices">
+                <div class="col-md-4 col-sm-10 col-xs-12 imagesServices">
                     <img src="<?php print (site_url("assets/images/applicationsmobiles1.png")); ?>">
                 </div>
                 <div class="col-md-8 col-sm-12">
                     <p class="detailleServices">Conception et développement Android, iOS et Windows Phone | Applications natives ou hybrides, BtoC et BtoB couplées d’un système d’information | Interfaces ergonomiques et intuitives avec des fonctionnalités mobiles innovantes : QR Code NFC… Notre équipe de développeurs d’applications mobiles prend en charge tous types de projets. Pour votre première application mobile, choisissez une équipe qui sera attentive à toutes vos exigences.</p>
-                    <div class="row">
-                        <div class="col-sm-2"></div>
+                    <div class="row panelImagesappServ">
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/xamarin.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/xamarin.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/xamarin.jpg")); ?>"></div></div>
+                        <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/xamarin.jpg")); ?>"></div></div>
                     </div>
                 </div>
             </div>
@@ -52,13 +58,16 @@
             </div>
         </div>
         <div class="row detailllepage listservice">
-            <div class="col-md-4 col-sm-12 imagesServices">
+            <div class="col-md-4 col-sm-10 col-xs-12 imagesServices">
                 <img src="<?php print (site_url("assets/images/systemreseau.jpg")); ?>">
             </div>
             <div class="col-md-8 col-sm-12">
-                <p class="detailleServices">Conception et développement Android, iOS et Windows Phone | Applications natives ou hybrides, BtoC et BtoB couplées d’un système d’information | Interfaces ergonomiques et intuitives avec des fonctionnalités mobiles innovantes : QR Code NFC… Notre équipe de développeurs d’applications mobiles prend en charge tous types de projets. Pour votre première application mobile, choisissez une équipe qui sera attentive à toutes vos exigences.</p>
-                <div class="row">
-                    <div class="col-sm-2"></div>
+                <p class="detailleServices">Audit et expertise du système et réseaux pour des prestations en local et à l’international | Mise en place du réseau chez le client et configuration complète | Monitoring, sécurisation, optimisation, augmentation de la performance, suivi et maintenance 24h/24 et 7j/7 | Hébergement grâce aux partenariats sur la plateforme de Google Cloud et sur HISI. Notre équipe d'administrateurs assure votre système et réseaux sans interruption et propose des solutions sur mesure.</p>
+                <div class="row panelImagesappServ">
+                    <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/parallesdesktop.jpg")); ?>"></div></div>
+                    <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/Linux.jpg")); ?>"></div></div>
+                    <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/Linux.jpg")); ?>"></div></div>
+                    <div class="col-sm-2 col-xs-2"><div class="panelcontour" ><img src="<?php print (site_url("assets/images/Linux.jpg")); ?>"></div></div>
                 </div>
             </div>
         </div>
